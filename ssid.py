f377ab5f5f90c2eb18135e91301fd3c776a683a5
Andy <andy.lamperski@gmail.com>
2016-04-20 13:24:41 -0500

    added stability constraint and tests

--- ssid.py
+++ ssid.py
@@ -1,6 +1,15 @@
 import numpy as np
 import scipy.linalg as la
 
+try:
+    import cvxpy as cvx
+    hasCvx = True
+
+except ImportError:
+    hasCvx = False
+    
+
+
 def generalizedPlant(A,B,C,D,Cov,dt):
     CovChol = la.cholesky(Cov,lower=True)
     NumStates = len(A)
@@ -106,43 +115,13 @@ def getHankelMatrices(x,NumRows,NumCols,blockWidth=1):
     XFuture = blockHankel(xFutureLeft,x[:,2*NumRows-1:2*NumRows-1+NumCols])
     return XPast,XFuture
 
-def N4SID(u,y,NumRows,NumCols,NSig):
-    """
-    A,B,C,D,Cov,Sigma = N4SID(u,y,NumRows,NumCols,n)
-
-    Let NumVals be the number of input and output values available
-    In this case:
-
-    u - NumInputs x NumVals array of inputs
-    y - NumOutputs x NumVals array of outputs
-
-    NumRows - Number of block rows in the past and future block Hankel matrices
-    NumCols - Number of columns in the past and future block Hankel matrices
-
-    n - desired state dimension.
-
-    For the algorithm to work, you must have:
-
-    NumVals >= 2*NumRows + NumCols - 1
-
-    Returns
-
-    A,B,C,D - the state space realization from inputs to outputs
+def preProcess(u,y,NumDict):
     
-    Cov - the joint covariance of the process and measurement noise
-
-    Sigma - the singular values of the oblique projection of 
-            row space of future outputs along row space of 
-            future inputs on the row space of past inputs and outputs.
-            
-            Examining Sigma can be used to determine the required state 
-            dimension
-
-    """
     NumInputs = u.shape[0]
     NumOutputs = y.shape[0]
-    
-
+    NumRows = NumDict['Rows']
+    NumCols = NumDict['Columns']
+    NSig = NumDict['Dimension']
     UPast,UFuture = getHankelMatrices(u,NumRows,NumCols)
     YPast,YFuture = getHankelMatrices(y,NumRows,NumCols)
     Data = np.vstack((UPast,UFuture,YPast))
@@ -175,21 +154,39 @@ def N4SID(u,y,NumRows,NumCols,NSig):
     GamData = np.vstack((GamSolve,UFuture))
 
     GamYData = np.vstack((GamShiftSolve,YFuture[:NumOutputs]))
+    # Should probably move to a better output structure
+    # One that doesn't depent so heavily on ordering
 
-    K = la.lstsq(GamData.T,GamYData.T)[0].T
+    GammaDict = {'Data':GamData,
+                 'DataLess':GammaLess,
+                 'DataY':GamYData,
+                 'Pinv': GammaPinv,
+                 'LessPinv': GammaLessPinv}
+    return GammaDict,S
+
+def postProcess(K,GammaDict,NumDict):
+    GamData = GammaDict['Data']
+    GamYData = GammaDict['DataY']
+
     rho = GamYData - np.dot(K,GamData)
 
+    NSig = NumDict['Dimension']
     AID = K[:NSig,:NSig]
     CID = K[NSig:,:NSig]
     
 
+    NumCols = NumDict['Columns']
     CovID = np.dot(rho,rho.T) / NumCols
     
     # Now we must construct B and D
 
+    GammaPinv = GammaDict['Pinv']
+    GammaLessPinv = GammaDict['LessPinv']
     AC = np.vstack((AID,CID))
     L = np.dot(AC,GammaPinv)
 
+    NumRows = NumDict['Rows']
+    NumOutputs = NumDict['Outputs']
     M = np.zeros((NSig,NumRows*NumOutputs))
     M[:,NumOutputs:] = GammaLessPinv
     Mleft = blockTranspose(M,NSig,NumOutputs)
@@ -200,8 +197,10 @@ def N4SID(u,y,NumRows,NumCols,NSig):
     NBot= -blockHankel(LbotLeft,blockHeight=NumOutputs)
     NBot[:NumOutputs,:NumOutputs] = NBot[:NumOutputs,:NumOutputs] + np.eye(NumOutputs)
 
+    GammaLess = GammaDict['DataLess']
     N = np.dot(np.vstack((NTop,NBot)),la.block_diag(np.eye(NumOutputs),GammaLess))
-    
+
+    NumInputs = NumDict['Inputs']
     KsTop = np.zeros((NSig*NumRows,NumInputs))
     KsBot = np.zeros((NumOutputs*NumRows,NumInputs))
 
@@ -216,5 +215,96 @@ def N4SID(u,y,NumRows,NumCols,NSig):
 
     BID = DB[NumOutputs:]
     DID = DB[:NumOutputs]
+
+    return AID,BID,CID,DID,CovID
+
+
+def N4SID(u,y,NumRows,NumCols,NSig,require_stable=False):
+    """
+    A,B,C,D,Cov,Sigma = N4SID(u,y,NumRows,NumCols,n,require_stable=False)
+
+    Let NumVals be the number of input and output values available
+    In this case:
+
+    u - NumInputs x NumVals array of inputs
+    y - NumOutputs x NumVals array of outputs
+
+    NumRows - Number of block rows in the past and future block Hankel matrices
+    NumCols - Number of columns in the past and future block Hankel matrices
+
+    n - desired state dimension.
+
+    For the algorithm to work, you must have:
+
+    NumVals >= 2*NumRows + NumCols - 1
+
+    Returns
+
+    A,B,C,D - the state space realization from inputs to outputs
+    
+    Cov - the joint covariance of the process and measurement noise
+
+    Sigma - the singular values of the oblique projection of 
+            row space of future outputs along row space of 
+            future inputs on the row space of past inputs and outputs.
+            
+            Examining Sigma can be used to determine the required state 
+            dimension
+
+    require_stable - An optional boolean parameter. Default is False
+                     If False, the standard N4SID algorithm is used
+                     If True, the state matrix, A, 
+                     will have spectral radius < 1.
+                     In order to run with require_stable=True, cvxpy 
+                     must be installed. 
+           
+
+    """
+    NumInputs = u.shape[0]
+    NumOutputs = y.shape[0]
+
+    
+    NumDict = {'Inputs': NumInputs,
+               'Outputs': NumOutputs,
+               'Dimension':NSig,
+               'Rows':NumRows,
+               'Columns':NumCols}
+    
+    GammaDict,S = preProcess(u,y,NumDict)
+
+
+    GamData = GammaDict['Data']
+    GamYData = GammaDict['DataY']
+
+    if not require_stable:
+        K = la.lstsq(GamData.T,GamYData.T)[0].T
+    else:
+
+        Kvar = cvx.Variable(NSig+NumOutputs,NSig+NumInputs*NumRows)
+
+        Avar = Kvar[:NSig,:NSig]
+
+        Pvar = cvx.Semidef(NSig)
+
+        LyapCheck = cvx.vstack(cvx.hstack(Pvar,Avar),
+                               cvx.hstack(Avar.T,np.eye(NSig)))
+
+        Constraints = [LyapCheck>>0,Pvar << np.eye(NSig)]
+
+        diffVar = GamYData - Kvar*GamData
+        objFun = cvx.norm(diffVar,'fro')
+        Objective = cvx.Minimize(objFun)
+
+        Prob = cvx.Problem(Objective,Constraints)
+
+        result = Prob.solve()
+
+        K = Kvar.value
+
+        
+    
+    AID,BID,CID,DID,CovID = postProcess(K,GammaDict,NumDict)
+
+    
     
     return AID,BID,CID,DID,CovID,S
